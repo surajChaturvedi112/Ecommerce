b5ba0a6a2c214e9e09a902cd196dca45c3388a7f
suraj chaturvedi <surajchaturvedi1111@gmail.com>
2024-02-07 15:55:16 +0530

    first commit

---
+++ Backend/README.md
@@ -0,0 +1,3 @@
+# to start this 
+
+npm start
---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("./db/config");
+const User =require("./db/User")
+const app = express();
+const cors = require("cors")
+app.use(express.json());
+
+app.use(cors());
+
+app.post("/register", async (req, res) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    res.send(result)
+})
+
+app.post("/login", async (req, res) => {
+    let user = await User.findOne(req.body).select("-password");
+    if(req.body.email && req.body.password){
+        user ? res.send(user) : res.send({result: "No user found"})     
+    }else{
+        res.send({result: "No user found"})
+    }
+})
+
+app.listen(5000, () => {
+    console.log("server is running on port 5000");
+});
+
